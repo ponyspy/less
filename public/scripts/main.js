6477622b71f997d041f91975bb55f137da392e1f
ponyspy <desade4me@gmail.com>
2013-04-03 13:06:49 +0400

    validate forms

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,4 +16,47 @@ $(document).ready(function() {
 	});
 	$('.scroll').jScrollPane();
 	$('.scroll_item').jScrollPane();
+	$('.f2').validate({
+		rules: {
+			login: {
+				required: true,
+				minlength: 2
+			},
+			name: {
+				required: true,
+				minlength: 2
+			},
+			password: {
+				required: true,
+				minlength: 5
+			},
+			skype: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required: true,
+				email: true
+			},
+		},
+		messages: {
+			login: {
+				required: "Введите имя пользователя",
+				minlength: "Не менее 2 символов"
+			},
+			name: {
+				required: "Введите имя",
+				minlength: "Не менее 2 символов"
+			},
+			password: {
+				required: "Введите пароль",
+				minlength: "Не менее 5 символов"
+			},
+			email: "Введите правильный email",
+			skype: {
+				required: "Введите имя в Skype",
+				minlength: "Не менее 2 символов"
+			}
+		}
+	});
 });
